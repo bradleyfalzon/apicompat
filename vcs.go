ebd65f0a7615c46c9bf42ef0a0461dc13efe81c9
Olivier Gagnon <oli.gagnon4418@gmail.com>
2016-08-14 18:17:28 -0400

    fileInfo moved back to non-test code

--- vcs.go
+++ vcs.go
@@ -149,3 +149,40 @@ func (fi fileInfo) IsDir() bool { return fi.dir }
 
 // Sys is one of the method needed to implement os.FileInfo
 func (fi fileInfo) Sys() interface{} { panic("not implemented") }
+
+// guarantee at compile time that strVCS implements VCS
+var _ VCS = (*strVCS)(nil)
+
+// strVCS provides a in memory vcs used for testing, but does not support
+// subdirectories.
+type strVCS struct {
+	files map[string]map[string][]byte // revision -> path -> contents
+}
+
+// SetFile contents for a particular revision and path
+func (v *strVCS) SetFile(revision, path string, contents []byte) {
+	if v.files == nil {
+		v.files = make(map[string]map[string][]byte)
+	}
+	if _, ok := v.files[revision]; !ok {
+		v.files[revision] = make(map[string][]byte)
+	}
+	v.files[revision][path] = contents
+}
+
+func (v strVCS) ReadDir(revision, path string) (files []os.FileInfo, err error) {
+	for file := range v.files[revision] {
+		files = append(files, fileInfo{
+			name: file,
+		})
+	}
+	return files, nil
+}
+
+func (v strVCS) OpenFile(revision, path string) (io.ReadCloser, error) {
+	return ioutil.NopCloser(bytes.NewReader(v.files[revision][filepath.Base(path)])), nil
+}
+
+func (strVCS) DefaultRevision() (string, string) {
+	return "rev1", "rev2"
+}
